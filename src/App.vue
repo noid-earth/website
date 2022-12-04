f4e6044c650a62e0adadfb16897b32555dd145e3
jurgenjacobsen <jurgenjacobsen@outlook.com>
2022-12-04 23:51:57 +0000

    ...

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+  import Header from "./components/Header.vue";
+  import Footer from "./components/Footer.vue";
+</script>
+
+<template>
+
+  <header class="container mx-auto">
+    <Header/>
+  </header>
+
+  <main>
+    
+    <div id="" class="h-96 container mx-auto px-4 mt-6">
+      <h1 class="text-4xl text-white">A programming organization</h1>
+        <p class="w-1/2 opacity-70">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae sint impedit modi excepturi sequi ducimus. Aut maxime eligendi fugiat porro itaque voluptatum corrupti voluptate ducimus laudantium, quibusdam officia praesentium eum velit doloribus? Quas maiores fuga nesciunt!</p>
+    </div>
+
+    <div id="projects" class="h-96 bg-[#292929]">
+
+    </div>
+
+    <div id="about" class="h-96 container mx-auto">
+
+    </div>
+
+    <div id="contact" class="h-96 bg-[#292929]">
+
+    </div>
+
+  </main>
+
+  <footer>
+    <Footer/>
+  </footer>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav class="py-2.5 rounded-lg">
+      <div class="flex flex-wrap items-center justify-between">
+
+        <a href="/">
+          <span class="text-xl font-semibold ml-4">project<span class="text-accent">Dema</span></span>
+        </a>
+
+        <div class="items-center justify-between w-auto flex">
+          <ul class="flex flex-col p-4 mt-4 border md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0">
+            <li>
+              <a href="#" class="">Home</a>
+            </li>
+            <li>
+              <a href="#projects" class="">Projects</a>
+            </li>
+            <li>
+              <a href="#" class="">About</a>
+            </li>
+            <li>
+              <a href="#" class="">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+</template>
